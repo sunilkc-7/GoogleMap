908e921a9b17ebf180884334de71823c65beff8a
sunilkc-7 <sunilkc2055@gmail.com>
2023-02-04 19:39:32 +0545

    google map integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+const key = process.env.REACT_APP_GOOGLE_KEY;
 
-function App() {
+function MapContainer(props) {
+  const [selectedPlace, setSelectedPlace] = useState({});
+  const [activeMarker, setActiveMarker] = useState({});
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
+
+  const onMarkerClick = (props, marker, e) => {
+    setSelectedPlace(props);
+    setActiveMarker(marker);
+    setShowingInfoWindow(true);
+  };
+
+  const onMapClicked = () => {
+    if (showingInfoWindow) {
+      setShowingInfoWindow(false);
+      setActiveMarker(null);
+    }
+  };
+
+  const handleSearch = async (e) => {
+    // const searchValue = e.target.value;
+
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${search}&key=${key}`
+    );
+    const data = await response.json();
+    setData(data.results[0]);
+
+    setSelectedPlace({
+      name: search,
+      position: {
+        lat: data.results[0].geometry.location.lat,
+        lng: data.results[0].geometry.location.lng,
+      },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "30%",
+          left: "0",
+          border: "1px solid red",
+          zIndex: "99",
+          padding: "30px 60px",
+          background: "white",
+          borderRadius: "25px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "4px",
+        }}
+      >
+        <span>latitude:{data?.geometry?.location.lat}</span>
+        <span>longitude:{data?.geometry?.location.lng}</span>
+      </div>
+
+      <input
+        style={{
+          marginTop: "10px",
+          marginBottom: "5px",
+        }}
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Enter location"
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      <Map
+        google={props.google}
+        onClick={onMapClicked}
+        style={{ width: "100%", height: "100%" }}
+        zoom={5}
+        center={{
+          lat: selectedPlace.position ? selectedPlace.position.lat : 27.717245,
+          lng: selectedPlace.position ? selectedPlace.position.lng : 85.323959,
+        }}
+      >
+        <Marker
+          onClick={onMarkerClick}
+          name={selectedPlace.name || "Kathmandu"}
+          position={selectedPlace.position}
+        />
+        <InfoWindow marker={activeMarker} visible={showingInfoWindow}>
+          <div>
+            <h1>{search}</h1>
+            <p>
+              Latitude:{""}
+              {selectedPlace.position ? selectedPlace.position.lat : "N/A"}
+            </p>
+            <p>
+              Longitude:{""}
+              {selectedPlace.position ? selectedPlace.position.lng : "N/A"}
+            </p>
+          </div>
+        </InfoWindow>
+      </Map>
     </div>
   );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: key,
+})(MapContainer);
